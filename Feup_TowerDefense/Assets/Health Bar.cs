389c5b6a00fa73cec4e9b6d260e09774387800f5
daviici <davidvan93@gmail.com>
2015-03-21 18:22:27 +0000

    health, not work

---
+++ Feup_TowerDefense/Assets/Health Bar.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBar : MonoBehaviour {
+	public Tower tower;
+	public Transform ForegroundSprite;
+	public SpriteRenderer ForegroundRendered; // para trocar a cor da barra a medida que vai ate ao zero
+	public Color MaxHelathColor = new Color (255 / 255f, 63 / 255f, 63 / 255f);
+	public Color MinHealthColor = new Color (64 / 255f, 137 / 255f, 255 / 255f);
+	
+	public void Update(){
+		var healthPercent = Tower.Health / (float) Tower.MaxHealth;
+		
+		ForegroundSprite.localScale = new Vector3 (healthPercent, 1, 1);
+		ForegroundRendered.color = Color.Lerp (MaxHelathColor, MinHealthColor, healthPercent);// retorna uma cor entre o max e min de acordo com a distancia
+	}
+}
+
+
---
+++ Feup_TowerDefense/Assets/Tower.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Tower : MonoBehaviour {
+	
+	/*health of player*/
+	public int MaxHealth = 100;
+	public GameObject OuchEffect; // quando o jogador recebe "damage"
+	
+	
+	public int Health{ get; private set; }
+	
+	public void Awake(){
+		/*health of player*/
+		Health = MaxHealth;
+		/*---*/
+	}
+
+	public void Update(){
+
+	}
+
+	/*health of player*/
+	public void TakeDamage(int damage, GameObject instigator){
+	
+	}
+}
