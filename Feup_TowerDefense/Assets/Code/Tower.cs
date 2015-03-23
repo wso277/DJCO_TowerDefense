13824cf02cd887a1084dbf6bc423bf45bf8ef0da
daviici <davidvan93@gmail.com>
2015-03-23 12:41:32 +0000

    projectile on tower

--- Feup_TowerDefense/Assets/Code/Tower.cs
+++ Feup_TowerDefense/Assets/Code/Tower.cs
@@ -7,14 +7,11 @@ public class Tower : MonoBehaviour {
 	/*health of player*/
 	public int MaxHealth = 100;
 	public GameObject OuchEffect; // quando o jogador recebe "damage"
-<<<<<<< HEAD
 	public Projectile Projectile;
 	private Vector2 _direction;
 	private Vector2 _velocity;
-=======
 	public float fireRate = 100;
 	public float range = 4;
->>>>>>> origin/master
 
 	public int Health{ get; private set; }
 	public bool IsDead { get; private set; }
