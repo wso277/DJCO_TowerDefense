ee9daaaa77d3ff6d3b6cffa7376abad8f625e1de
daviici <davidvan93@gmail.com>
2015-03-23 12:32:43 +0000

    projectile to tower

--- Feup_TowerDefense/Assets/Code/Tower.cs
+++ Feup_TowerDefense/Assets/Code/Tower.cs
@@ -7,6 +7,9 @@ public class Tower : MonoBehaviour {
 	/*health of player*/
 	public int MaxHealth = 100;
 	public GameObject OuchEffect; // quando o jogador recebe "damage"
+	public Projectile Projectile;
+	private Vector2 _direction;
+	private Vector2 _velocity;
 
 	public int Health{ get; private set; }
 	public bool IsDead { get; private set; }
@@ -18,6 +21,7 @@ public class Tower : MonoBehaviour {
 	}
 	
 	public void Update(){
-
+		var projectile = (Projectile)Instantiate (Projectile, transform.position, transform.rotation);
+		projectile.Initialize (gameObject, _direction, _velocity);
 	}
 }
