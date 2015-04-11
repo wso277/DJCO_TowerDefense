3c56eee2e9daaafe074820c75fb77f5cfea3103e
Pedro Tavares <p.miguel.tr@gmail.com>
2015-04-11 18:11:20 +0100

    added tower spawner

---
+++ Feup_TowerDefense/Assets/Tower1Selector.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tower1Selector : MonoBehaviour {
+
+	public Sprite normal;
+	public Sprite hover;
+	public Transform SpawnerPrefab;
+
+	private SpriteRenderer spriteRenderer; 
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer>();
+
+		if (spriteRenderer.sprite == null) {
+			spriteRenderer.sprite = normal;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseOver () {
+		spriteRenderer.sprite = hover;
+	}
+
+	void OnMouseExit () {
+		spriteRenderer.sprite = normal;
+	}
+
+	void OnMouseUp () {
+		Instantiate (SpawnerPrefab, this.transform.position, this.transform.rotation);
+	}
+}
---
+++ Feup_TowerDefense/Assets/Tower1Spawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tower1Spawner : MonoBehaviour {
+
+	private Vector3 mousePosition;
+	public float moveSpeed = 1f;
+	public Transform TowerPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		mousePosition = Input.mousePosition;
+		mousePosition.z = -7.84f;
+		mousePosition = Camera.main.ScreenToWorldPoint (mousePosition);
+		mousePosition.x = -mousePosition.x;
+		mousePosition.y = -mousePosition.y + 3;
+		mousePosition.z = 0;
+		//transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+		transform.position = mousePosition;
+		//Debug.Log ("x: " + mousePosition.x + " y: " + mousePosition.y + " z: " + mousePosition.z);
+
+		if (Input.GetMouseButtonDown (0)) {
+			Transform tower = (Transform)Instantiate (TowerPrefab, this.transform.position, this.transform.rotation);
+			Destroy(gameObject);
+		}
+	}
+}
