0575f4c34febfc85c0bfd24180bdcd70f5288e11
wso277 <oliveira.wilson93@gmail.com>
2015-04-12 14:50:00 +0100

    Changed interface

--- Feup_TowerDefense/Assets/Code/HUD.cs
+++ Feup_TowerDefense/Assets/Code/HUD.cs
@@ -10,11 +10,18 @@ public class HUD : MonoBehaviour {
 	private string healthText;
 	private GUIStyle style;
 
+    private Rect chargeTextArea;
+    private string chargeText;
+    private GUIStyle chargestyle;
+
 	void Start () {
 		logicScript = GameObject.Find("GameLogic").GetComponent<GameLogic>();
 		healthTextArea = new Rect(30,30,Screen.width, Screen.height);
 		style = new GUIStyle();
 		style.fontSize = 20;
+        chargeTextArea = new Rect(700, 20, Screen.width, Screen.height);
+        chargestyle = new GUIStyle();
+        chargestyle.fontSize = 20;
 	}
 	
 	// Update is called once per frame
@@ -26,5 +33,14 @@ public class HUD : MonoBehaviour {
 		healthText = logicScript.currentLifePoints.ToString() + "/" + logicScript.totalLifePoints.ToString();
 		GUI.contentColor = Color.black;
 		GUI.Label(healthTextArea,healthText,style);
+        if (logicScript.towerCharges == 1)
+        {
+            chargeText = "You can place " + logicScript.towerCharges + " tower";
+        }
+        else
+        {
+            chargeText = "You can place " + logicScript.towerCharges + " towers";
+        }
+        GUI.Label(chargeTextArea, chargeText, chargestyle);
 	}
 }
